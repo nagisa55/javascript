17e883dde2dbd94d9dd817b8097cf5606f695ba5
github nagisa55 <kamomeeee00@yahoo.co.jp>
2021-10-15 16:20:57 +0900

    create js file

---
+++ typinggame/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+{
+
+  const words = [
+    'red',
+    'blue',
+    'pink',
+  ];
+
+  let word;
+  let loc = 0;
+
+  const target = document.getElementById('target');
+  word = words[Math.floor(Math.random() * words.length)]
+  target.textContent = word;
+
+  document.addEventListener('keydown', e => {
+    if(e.key !== word[loc]){
+      return;
+    }
+    loc++;
+    target.textContent = '_'.repeat(loc) + word.substring(loc);
+  });
+}
